d3810438c77246d5d15f0319c53616281160be41
JGSims <josi3006@hotmail.com>
2020-02-07 22:24:11 -0500

    Working on Delete functions

--- tracker.js
+++ tracker.js
@@ -1,14 +1,20 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
 
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: process.env.PORT || 3000,
+    port: 3306,
     user: 'root',
     password: 'password',
     database: 'EmployeeData_DB'
 });
+
+
+
+
 
 
 
@@ -25,7 +31,7 @@ function taskPrompt() {
         .prompt({
             name: 'action',
             type: 'rawlist',
-            message: 'What would you like to do?',
+            message: 'Would you like to View, Add, Change or Delete data?',
             choices: [
                 'View Employee/Department Data',
                 'Add a New Employee, Department or Role',
@@ -41,7 +47,7 @@ function taskPrompt() {
                     break;
 
                 case 'Add a New Employee, Department or Role':
-                    addNew();
+                    addData();
                     break;
 
                 case 'Change an Existing Department, Role or Employee':
@@ -49,7 +55,7 @@ function taskPrompt() {
                     break;
 
                 case 'Delete a Department, Role or Employee':
-                    deleteData();
+                    delData();
                     break;
 
                 case 'Quit':
@@ -62,6 +68,7 @@ function taskPrompt() {
 
 
 
+// This function prompts user for data to VIEW
 
 function viewData() {
     inquirer
@@ -73,26 +80,24 @@ function viewData() {
                 'View All Departments',
                 'View All Roles',
                 'View All Employees',
-                'Go Back a Step',
                 'Quit'
             ]
         })
         .then(function (answer) {
             switch (answer.action) {
                 case 'View All Departments':
-                    viewDepts();
+                    var viewType = 'departments';
+                    viewQuery(viewType);
                     break;
 
                 case 'View All Roles':
-                    viewRoles();
+                    var viewType = 'roles';
+                    viewQuery(viewType);
                     break;
 
                 case 'View All Employees':
-                    viewEmpls();
-                    break;
-
-                case 'Go Back a Step':
-                    taskPrompt();
+                    var viewType = 'employees';
+                    viewQuery(viewType);
                     break;
 
                 case 'Quit':
@@ -102,3 +107,181 @@ function viewData() {
             }
         });
 }
+
+
+// This function displays data the user requested
+
+function viewQuery(viewType) {
+    // var query = "SELECT * FROM ?";
+    connection.query('SELECT * FROM ' + viewType, function (err, res) {
+
+        console.log('Here are all the Things!');
+        console.table(res);
+
+        taskPrompt();
+
+        // return process.exit(22);
+
+    })
+}
+
+
+
+
+
+// This function prompts the user for data to ADD
+
+// function addData() {
+//     inquirer
+//         .prompt({
+//             name: 'action',
+//             type: 'rawlist',
+//             message: 'Would you like to ADD a Department, Role or Employee?',
+//             choices: [
+//                 'Add a new Department',
+//                 'Add a new Role',
+//                 'Add a new Employee',
+//                 'Quit'
+//             ]
+//         })
+//         .then(function (answer) {
+//             switch (answer.action) {
+//                 case 'Add a new Department':
+//                     var viewType = 'departments';
+//                     viewQuery(viewType);
+//                     break;
+
+//                 case 'Add a new Role':
+//                     var viewType = 'roles';
+//                     viewQuery(viewType);
+//                     break;
+
+//                 case 'Add a new Employee':
+//                     var viewType = 'employees';
+//                     viewQuery(viewType);
+//                     break;
+
+//                 case 'Quit':
+//                     console.log('Quitter.');
+//                     taskPrompt();
+//                     break;
+//             }
+//         });
+// }
+
+
+
+
+
+// function viewQuery(viewType) {
+//     // var query = "SELECT * FROM ?";
+//     connection.query('SELECT * FROM ' + viewType, function (err, res) {
+
+//         console.log('Here are all the Things!');
+//         console.table(res);
+
+//         taskPrompt();
+
+//         // return process.exit(22);
+
+//     })   
+// }
+
+
+// This function prompts user for data to DELETE
+
+function delData() {
+    inquirer
+        .prompt([{
+            name: 'bucket',
+            type: 'list',
+            message: 'What would you like to DELETE?',
+            choices: [
+                'Department',
+                'Role',
+                'Employee',
+                'I\'d like to Quit'
+            ]
+        },
+        {
+            name: 'drop',
+            type: 'input',
+            message: 'Which one would you like to delete?'
+        }])
+        .then(function (answer) {
+
+            switch (answer.bucket) {
+                case 'Department':
+                    var drop = answer.drop
+                    deleteFunc(answer.bucket, drop);
+                    break;
+
+                case 'Role':
+                    var query = 'SET SQL_SAFE_UPDATES=0;' +
+                        'DELETE FROM roles WHERE roletitle = ' + answer.drop +
+                        '; SET SQL_SAFE_UPDATES=1;';
+
+                    deleteFunc(query);
+                    break;
+
+                case 'Employee':
+                    var query = 'SET SQL_SAFE_UPDATES=0;' +
+                        'DELETE FROM employees WHERE lastname = ' + answer.drop +
+                        '; SET SQL_SAFE_UPDATES=1;';
+
+                    deleteFunc(query);
+                    break;
+
+                case 'I\'d like to Quit':
+                    console.log('Quitter.');
+                    taskPrompt();
+                    break;
+            }
+
+            function deleteFunc(query) {
+                connection.query(query, function (err, res) {
+                    if (err) throw err;
+                });
+            }
+        })
+}
+
+
+// // This function displays data the user requested
+
+// function delQuery(delTable, delName) {
+//     console.log('at top: ' + delTable);
+//     console.log('at top: ' + delName);
+
+
+//     inquirer
+//         .prompt({
+//             name: 'drop',
+//             type: 'input',
+//             message: 'Which ' + delName + ' do you want to DELETE from ' + delTable + '?'
+//         })
+//         .then(function (answer, delTable, delName) {
+
+//             console.log('at query ' + delTable);
+//             console.log('at query ' + delName);
+//             console.log('at query ' + answer.drop);
+
+//             const delData = 'SET SQL_SAFE_UPDATES=0;' +
+//                 'DELETE FROM ' + delTable + ' WHERE ' + delName + ' = ' + answer.drop +
+//                 '; SET SQL_SAFE_UPDATES=1;';
+
+//             connection.query(delData, function (err, res) {
+//                 if (err) throw err;
+
+
+//                 console.log('Successfully deleted ' + answer.drop);
+
+//                 taskPrompt();
+
+//                 // return process.exit(22);
+
+//             })
+//         })
+// }
+
+
